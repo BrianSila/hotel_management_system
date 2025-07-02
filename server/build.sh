8cadb83f30fe5f0655bf88bbf09f04460cc0a284
Brian <brian.sila1@student.moringaschool.com>
2025-07-02 07:55:07 +0300

    build.sh

--- server/build.sh
+++ server/build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+# Install build-essential and python development headers
+apt-get update && apt-get install -y build-essential python3-dev
+
+# Set the path for the Rust compiler (still needed)
 export PATH=/usr/local/cargo/bin:$PATH
 
+# Install python dependencies
 pip install -r requirements.txt
